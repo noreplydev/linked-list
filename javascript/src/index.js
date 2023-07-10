c9f3dd9bc3f9159233aeee863644587ab3fda2e0
noreplydev <contact.cristiansanchez@gmail.com>
2023-07-10 22:40:40 +0200

    [FT](javascript) Working on insertion tests

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,5 +1,16 @@
 import LinkedList from './core/LinkedList.js'
 
+
+export function append(linkedList) {
+  
+}
+
+export function preppend(linkedList) {
+  
+}
+
+export function insertAt(linkedList) {
+  
+}
+
 const linkedList = new LinkedList()
-
-
